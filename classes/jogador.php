5811215df65add2e63a63b2e6bc8f89ef37103d1
douglasbernardo <43753293+douglasbernardo@users.noreply.github.com>
2020-01-03 20:25:56 -0300

    basquete e futebol

--- classes/jogador.php
+++ classes/jogador.php
@@ -1,14 +1,19 @@
 <?php 
 
 class Jogador{
-    private $nome;
+    public $nome;
     private $posicao;
-    private $preco;
+    protected $preco;
 
     public function Apresentarjogador(){
         echo " Jogador : " .$this->nome."<br>";
         echo " Posição : " .$this->getPosicao()."<br>";
         echo "Preço do jogador : " .$this->preco;
+    }
+    
+    //acessando o metodo privador Comprar
+    public function MostrarCompra(){
+        $this->Comprar();
     }
 
     function __construct(string $nome, string $posicao, $preco)
@@ -42,7 +47,8 @@ class Jogador{
         $this->posicao = $m;
     }
 
-    public function Comprar()
+    //metodo privado
+    private function Comprar()
     {
         if($this->preco >= "50.000.00"){
             echo "Jogador " .$this->nome. " esta muito caro";
@@ -53,7 +59,71 @@ class Jogador{
     }
 }
 
-$jogador = New Jogador("Van dijk","Zagueiro","30.000.000");
+ //herença pegando metodos e atributos daa classe Jogador
+ class Basquete extends Jogador{
+
+    public $nome;
+    private $posicao;
+    protected $preco;
+
+    public function Apresentarjogador(){
+        echo " Jogador : " .$this->nome."<br>";
+        echo " Posição : " .$this->getPosicao()."<br>";
+        echo "Preço do jogador : " .$this->preco;
+    }
+
+    public function MostrarCompra(){
+        $this->Comprar();
+    }
+
+    function __construct(string $nome, string $posicao, $preco)
+    {
+        $this->nome = $nome;
+        $this->posicao = $posicao;
+        $this->setPreco($preco);
+    }
+
+    public function getNome(){
+        return $this->nome;
+    }
+    public function getPreco()
+    {
+        return $this->preco;
+    }
+    public function getPosicao()
+    {
+        return $this->posicao;
+    }
+    public function setNome($n)
+    {
+        $this->nome = $n;
+    }
+    public function setPreco($p)
+    {
+        $this->preco = $p;
+    }
+    public function setPosicao($m)
+    {
+        $this->posicao = $m;
+    }
+
+    //metodo privado
+    private function Comprar()
+    {
+        if($this->preco >= "50.000.00"){
+            echo "Jogador " .$this->nome. " esta muito caro";
+        }
+        else{
+            echo "Jogador esta com preço bom";
+        }
+    }
+ }
+ 
+echo "Jogador de Futebol";
+$jogador = New Jogador("Van dijk","Zagueiro","80.000.000");
 $jogador->Apresentarjogador();
+echo "Jogador de Basquete";
+$basquete = new Jogador("LeBron James","Ala","60.000.000");
+$basquete->Apresentarjogador();
 echo "<hr>";
-$jogador->Comprar();
+$jogador->MostrarCompra();
